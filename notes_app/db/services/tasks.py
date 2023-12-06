b4d1066900efc5f1ec5c3af1d65600d83c9e8cc0
RastaBoy <trustyone3224@gmail.com>
2023-12-06 16:15:06 +0700

    Рефакторинг

--- notes_app/db/services/tasks.py
+++
@@ -1,6 +0,0 @@
-from .abc import IService
-from ..models.task import TaskModel
-
-
-class TaskService(IService[TaskModel]):
-    ...
--- notes_app/db/services/users.py
+++
@@ -1,6 +0,0 @@
-from .abc import IService
-from ..models.user import UserModel
-
-
-class UserService(IService[UserModel]):
-    ...
